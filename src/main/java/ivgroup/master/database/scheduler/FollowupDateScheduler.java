e0cc98a812e54c949d5270b2d55d382907b7f786
purav7838 <44437936+purav7838@users.noreply.github.com>
2020-05-21 15:37:04 +0530

    Scheduler Modification

--- src/main/java/ivgroup/master/database/scheduler/FollowupDateScheduler.java
+++ src/main/java/ivgroup/master/database/scheduler/FollowupDateScheduler.java
@@ -1,25 +1,15 @@
 package ivgroup.master.database.scheduler;
 
-import java.sql.Date;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import ivgroup.master.database.dao.impl.EmailLogDAOImpl;
-import ivgroup.master.database.dao.impl.TicketDAOImpl;
-import ivgroup.master.database.dto.logs.EmailLogInsert;
 
 @Configuration
 @EnableScheduling
 public class FollowupDateScheduler 
 {
-	
+	Thread t=null;
 	
 	@Scheduled(cron = "0 0 0 * * ?")
 	public void schedulerForFollowupDate()
