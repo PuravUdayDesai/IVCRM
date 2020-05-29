4395067f11931f17efff3dee4fae134f6b455895
purav7838 <44437936+purav7838@users.noreply.github.com>
2020-05-29 13:34:45 +0530

    Removed SRC folder

--- src/main/java/ivgroup/master/database/dao/schema/CRMAccessListDAO.java
+++
@@ -1,8 +0,0 @@
-package ivgroup.master.database.dao.schema;
-
-import java.sql.SQLException;
-
-public interface CRMAccessListDAO
-{
-	public Long 	checkExecutiveOwnerFlag(Long correspondingId)	throws SQLException,ClassNotFoundException;
-}
