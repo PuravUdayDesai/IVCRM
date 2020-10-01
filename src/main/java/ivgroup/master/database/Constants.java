f755a427f17490bcf6bd1de0e8545a7f3f7a9979
purav Desai <44437936+purav7838@users.noreply.github.com>
2020-10-01 23:42:55 +0530

    Update Constants.java

--- src/main/java/ivgroup/master/database/Constants.java
+++ src/main/java/ivgroup/master/database/Constants.java
@@ -3,7 +3,7 @@ package ivgroup.master.database;
 public class Constants {
 	
 	//General Details
-	public final static String URL_TO_REQUEST="<URL_TO_REQUEST>";//http://localhost:8080
+	public final static String URL_TO_REQUEST="<URL_TO_REQUEST>";
 	public final static String URL_PUBLIC_REQUEST="*";
 	
 	//Server Details
